b761ad9e82feda931adec690d9e5e49e91ff43a3
Raghav <raghavd.bitmesra@gmail.com>
2025-06-18 20:31:13 +0530

    build fixes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ heading, text, tags, image }) => {
     return (
         <div className='relative bg-container rounded-[40px] w-full overflow-hidden'>
             <div className='absolute w-[209px] h-[165px] z-0 bg-accent rounded-full blur-[150px] bottom-10 left-1/2 -translate-x-1/2'></div>
-            <div className='w-full relative z-10 p-[32px] gap-[32px] grid grid-rows-2 md:grid-cols-2'>
+            <div className='w-full relative z-10 p-[32px] gap-[32px] grid grid-rows-2 grid-cols-none lg:grid-rows-none lg:grid-cols-2'>
                 <div>
                     <img className='w-full h-full' src={image} alt="img" />
                 </div>
